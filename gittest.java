ef5aa0d50bc527333134eb3ff82eec5f4f95bd82
A Nelson Britto <165551149+Nelsonbritto@users.noreply.github.com>
2025-10-04 18:46:33 +0530

    Update gittest.java email test for failure

--- gittest.java
+++ gittest.java
@@ -1,5 +1,5 @@
 public class gittest {
-    public static void main(String[] args) {  // <-- fix here
+    //public static void main(String[] args) {  // <-- fix here
         System.out.println("this is you third project");
         int a = 10;
         int b= 20;
