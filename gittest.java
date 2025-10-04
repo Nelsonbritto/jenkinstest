d4086ac3005cda07fad5c0135a6effba91b866b1
A Nelson Britto <165551149+Nelsonbritto@users.noreply.github.com>
2025-10-04 16:06:39 +0530

    Create gittest.java

---
+++ gittest.java
@@ -0,0 +1,10 @@
+public class gittest {
+    public static void main(String[] args) {  // <-- fix here
+        System.out.println("this is you third project");
+        int a = 10;
+        int b= 20;
+       
+        System.out.println("testing for push code in git and github");
+
+    }
+}
